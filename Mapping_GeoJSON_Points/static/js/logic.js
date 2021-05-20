8d46e68526d6c1df16caf64735180f126d2f20b8
Habtamu Molla <78656720+Habtamu-ai@users.noreply.github.com>
2021-05-20 18:11:58 -0400

    Updating the mapping geojson with dark and street options

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -1,57 +1,48 @@
-// Add console.log to check to see if our code is working.
-console.log("working");
-// Create the map object with center at the San Francisco airport.
-let map = L.map('mapid').setView([37.5, -122.5], 10);
+// Create the map object with center and zoom level.
+// Create the map object with a center and zoom level.
 
 // We create the tile layer that will be the background of our map.
-
-// Then we add our 'graymap' tile layer to the map.
-// We create the tile layer that will be the background of our map.
-//to change the background of the map change "streets-v11" with "dark-v10 
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
 });
+// We create the dark view tile layer that will be an option for our map.
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+// Create a base layer that holds both maps.
+let baseMaps = {
+  Street: streets,
+  Dark: dark
+};
+// Create the map object with center, zoom level and default layer.
+let map = L.map('mapid', {
+  center: [30, 30],
+  zoom: 2,
+  layers: [streets]
+})
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
 
-// Then we add our 'graymap' tile layer to the map.
+// Then we add our 'streets' tile layer to the map.
 streets.addTo(map);
-// Grabbing our GeoJSON data.
 
-//  // Add GeoJSON data.
-let sanFranAirport =
-{"type":"FeatureCollection","features":[{
-    "type":"Feature",
-    "properties":{
-        "id":"3469",
-        "name":"San Francisco International Airport",
-        "city":"San Francisco",
-        "country":"United States",
-        "faa":"SFO",
-        "icao":"KSFO",
-        "alt":"13",
-        "tz-offset":"-8",
-        "dst":"A",
-        "tz":"America/Los_Angeles"},
-        "geometry":{
-            "type":"Point",
-            "coordinates":[-122.375,37.61899948120117]}}
-]};
-L.geoJSON(sanFranAirport).addTo(map);
+// Accessing the airport GeoJSON URL
+let airportData = "https://raw.githubusercontent.com/Habtamu-ai/Mapping_Earthquakes/main/majorAirports.json";
 
 // Grabbing our GeoJSON data.
-L.geoJson(sanFranAirport, {
-    //// We turn each feature into a marker on the map.
-    //pointToLayer: function(feature, latlng) {
-    //  console.log(feature);
-    //  return L.marker(latlng)
-    //  //.bindPopup("<h2>" + feature.properties.city + "</h2>");
-    //  .bindPopup("<h1>" + feature.properties.name + "</h1> <hr> <h2>" + feature.properties.city + ',' + feature.properties.country + "</h2>");
-    //}
-  
-      onEachFeature: function(feature, layer) {
-        layer.bindPopup("<h1>" + "Airport code:" + feature.properties.faa + "</h1> <hr> <h2> Airport name:" + feature.properties.name + "</h2>");
-       }
-  
+d3.json(airportData).then(function(data) {
+  console.log(data);
+// Creating a GeoJSON layer with the retrieved data.
+//L.geoJson(data).addTo(map);
+//});
+L.geoJson(data, {
+  onEachFeature: function(feature, layer) {
+ layer.bindPopup("<h1>" + "Airport code:" + feature.properties.faa + "</h1> <hr> <h2> Airport name:" + feature.properties.name + "</h2>").addTo(map);
+  }
+});
 
-  }).addTo(map);
+});
