b788a96a1e38690e16e7707630f2a6c7321c02c4
Habtamu Molla <78656720+Habtamu-ai@users.noreply.github.com>
2021-05-21 14:29:30 -0400

    Addint the earthquake data and map

---
+++ Earthquakes_past7days/static/js/logicStep1.js
@@ -0,0 +1,57 @@
+// Create the map object with center and zoom level.
+// Create the map object with a center and zoom level.
+console.log("working");
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+// We create the dark view tile layer that will be an option for our map.
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+// Create a base layer that holds both maps.
+let baseMaps = {
+  "Streets": streets,
+  "Satellite": satelliteStreets
+};
+// Create the map object with center, zoom level and default layer.
+let map = L.map('mapid', {
+  center: [39.57, -98.5],
+  zoom: 3,
+  layers: [streets]
+});
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
+
+// Then we add our 'streets' tile layer to the map.
+//streets.addTo(map);
+
+// Accessing the Toronto neighborhoods GeoJSON URL.
+let torontoHoods = "https://raw.githubusercontent.com/Habtamu-ai/Mapping_Earthquakes/main/torontoNeighborhoods.json";
+
+// Retrieve the earthquake GeoJSON data.
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJson(data).addTo(map);
+});
+
+//// Grabbing our GeoJSON data.
+//d3.json(torontoHoods).then(function(data) {
+//  console.log(data);
+//// Creating a GeoJSON layer with the retrieved data.
+////L.geoJson(data).addTo(map);
+////});
+//L.geoJson(data, {
+//  color : "Blue",
+//  weight: 1,
+//onEachFeature: function(feature, layer) {
+//layer.bindPopup("<h1>" + "Neighborhood:" + feature.properties.AREA_NAME + "</h1>")
+//.addTo(map);
+// }
+//});
+//
+//});
